9fe233f2caea6d91c7e8033b0ace33a4973a1a8b
mmilner29 <madisonm4514@gmail.com>
2022-03-18 22:50:42 -0700

    check for null values

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,35 @@
 const submitBtn = document.querySelector("#submit-btn");
 const searchDiv = document.querySelector("#search-hist");
+const cityName = document.querySelector("#city-name");
 
 var fetchWeather = function() {
     fetch("https://api.openweathermap.org/data/2.5/weather?q=phoenix&units=imperial&appid=6033dc9f9b191ae2855b71b9c8996bcb")
-}
+    console.log('annoyed');
+};
 
-fetchWeather();
 
 var fetchForecast = function() {
     fetch("https://api.openweathermap.org/data/2.5/forecast?q=phoenix&units=imperial&appid=6033dc9f9b191ae2855b71b9c8996bcb")
-}
-
-fetchForecast();
-
-var citySearch = function() {
-    console.log("search");
+    console.log("forecast");
 };
 
-submitBtn.addEventListener("submit", citySearch())
+
+var citySearch = function(event) {
+    event.preventDefault();
+    // console.log("search");
+
+
+    var city = $(this).children().val();
+
+    if (city === null || city === "") {
+        alert("Please enter a valid city name");
+    } else {
+        $("<button></button>").text(city).addClass("btn btn-secondary btn-block mt-2 mb-2").appendTo("#search-hist");
+        localStorage.setItem('City'+ [city], city);
+    }
+    
+};
+
+// $("<button></button>").addClass("btn btn-secondary btn-block mt-2 mb-2").appendTo("#search-hist").val(localStorage.getItem('CityPhoenix'));
+
+submitBtn.addEventListener("submit", citySearch)
