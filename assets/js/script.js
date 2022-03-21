1d7260acb555cb9ad463b7b949ce679c3e1ae524
mmilner29 <madisonm4514@gmail.com>
2022-03-20 21:06:35 -0700

    target specific data from API

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,34 +1,56 @@
 const submitBtn = document.querySelector("#submit-btn");
 const searchDiv = document.querySelector("#search-hist");
 const cityName = document.querySelector("#city-name");
-
-var fetchWeather = function() {
-    fetch("https://api.openweathermap.org/data/2.5/weather?q=phoenix&units=imperial&appid=6033dc9f9b191ae2855b71b9c8996bcb")
-    console.log('annoyed');
-};
-
-
-var fetchForecast = function() {
-    fetch("https://api.openweathermap.org/data/2.5/forecast?q=phoenix&units=imperial&appid=6033dc9f9b191ae2855b71b9c8996bcb")
-    console.log("forecast");
-};
-
+const apiKey = '6033dc9f9b191ae2855b71b9c8996bcb';
 
 var citySearch = function(event) {
     event.preventDefault();
-    // console.log("search");
 
-
-    var city = $(this).children().val();
+    var city = $(this).children().val().trim();
 
     if (city === null || city === "") {
         alert("Please enter a valid city name");
     } else {
         $("<button></button>").text(city).addClass("btn btn-secondary btn-block mt-2 mb-2").appendTo("#search-hist");
         localStorage.setItem('City'+ [city], city);
+
     }
-    
+    fetchWeather(city);
+    fetchForecast(city);
 };
+
+
+var fetchWeather = function(cityName) {
+    weatherUrl = 'https://api.openweathermap.org/data/2.5/weather?q=' + cityName + '&units=imperial&appid=' + apiKey;
+    fetch(weatherUrl)
+    .then((response) => {
+        return response.json();
+    })
+    .then((response) => {
+        displayWeather(response);
+    });
+};
+
+
+var fetchForecast = function(cityName) {
+    forecastUrl = 'https://api.openweathermap.org/data/2.5/forecast?q=' + cityName + '&units=imperial&appid=' + apiKey;
+    fetch(forecastUrl)
+    .then((response) => {
+        return response.json();
+    })
+    .then((response) => {
+        displayForecast(response);
+    });
+};
+
+var displayWeather = function(response) {
+    console.log(response.main.temp);
+};
+
+var displayForecast = function(response) {
+    console.log(response.list);
+};
+
 
 // $("<button></button>").addClass("btn btn-secondary btn-block mt-2 mb-2").appendTo("#search-hist").val(localStorage.getItem('CityPhoenix'));
 
