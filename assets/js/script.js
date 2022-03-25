a500b11d306c84fa6bd419de7e6f0e9d16922b0f
mmilner29 <madisonm4514@gmail.com>
2022-03-24 20:55:29 -0700

    append forecast

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,6 +4,12 @@ const cityName = document.querySelector("#city-name");
 const weatherDiv = document.querySelector('#current-weather')
 const apiKey = '6033dc9f9b191ae2855b71b9c8996bcb';
 
+const cardOne = document.querySelector("#day-1");
+const cardTwo = document.querySelector("#day-2");
+const cardThree = document.querySelector("#day-3");
+const cardFour = document.querySelector("#day-4");
+const cardFive = document.querySelector("#day-5");
+
 var citySearch = function(event) {
     event.preventDefault();
 
@@ -45,7 +51,7 @@ var fetchForecast = function(cityName) {
 };
 
 var displayWeather = function(response) {
-    console.log(response.name);
+    // console.log(response.name);
     var cityNm = response.name
     $("<h2></h2>").text(cityNm).appendTo(weatherDiv);
     var ul = $("<ul></ul>").appendTo(weatherDiv);
@@ -55,7 +61,35 @@ var displayWeather = function(response) {
 };
 
 var displayForecast = function(response) {
-    console.log(response.list);
+    // console.log(response.list[0].dt_txt);
+    $("<h3></h3>").text(response.list[0].dt_txt).appendTo(cardOne);
+    var ulOne = $("<ul></ul>").appendTo(cardOne);
+    $("<li></li>").text('Temp: ' + response.list[0].main.temp).appendTo(ulOne);
+    $("<li></li>").text('Humidity: ' + response.list[0].main.humidity).appendTo(ulOne);
+    $("<li></li>").text('Wind: ' + response.list[0].wind.speed).appendTo(ulOne);
+
+    $("<h3></h3>").text(response.list[8].dt_txt).appendTo(cardTwo);
+    var ulTwo = $("<ul></ul>").appendTo(cardTwo);
+    $("<li></li>").text('Temp: ' + response.list[8].main.temp).appendTo(ulTwo);
+    $("<li></li>").text('Humidity: ' + response.list[8].main.humidity).appendTo(ulTwo);
+    $("<li></li>").text('Wind: ' + response.list[8].wind.speed).appendTo(ulTwo);
+
+    $("<h3></h3>").text(response.list[16].dt_txt).appendTo(cardThree);
+    var ulThree = $("<ul></ul>").appendTo(cardThree);
+    $("<li></li>").text('Temp: ' + response.list[16].main.temp).appendTo(ulThree);
+    $("<li></li>").text('Humidity: ' + response.list[16].main.humidity).appendTo(ulThree);
+    $("<li></li>").text('Wind: ' + response.list[16].wind.speed).appendTo(ulThree);
+
+    $("<h3></h3>").text(response.list[24].dt_txt).appendTo(cardFour);
+    var ulFour = $("<ul></ul>").appendTo(cardFour);
+    $("<li></li>").text('Temp: ' + response.list[24].main.temp).appendTo(ulFour);
+    $("<li></li>").text('Humidity: ' + response.list[24].main.humidity).appendTo(ulFour);
+    $("<li></li>").text('Wind: ' + response.list[24].wind.speed).appendTo(ulFour);
+
+    $("<h3></h3>").text(response.list[32].dt_txt).appendTo(cardFive);
+    var ulFive = $("<ul></ul>").appendTo(cardFive);
+    $("<li></li>").text('Temp: ' + response.list[32].main.temp).appendTo(ulFive);
+    $("<li></li>").text('Humidity: ' + response.list[32].wind.speed).appendTo(ulFive);
 };
 
 
